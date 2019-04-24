8dfd833cc10f6bfb27a801964a51636f550863d8
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-04-24 14:09:13 -0300

    refatoracao do servico auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,17 +4,20 @@ require('dotenv-safe').config()
 module.exports = {
     authenticate(user /*Object*/) {
         return new Promise((resolve, reject) => {
-            if (typeof user !== 'object')
-                reject(new Error('user must be an object'))
+            try {
+                if (typeof user !== 'object') reject(new Error('user must be an object'))
 
-            const token = JWT.sign(user, process.env.SECRET, {
-                expiresIn: 1800 //expires in 30min
-            })
+                const token = JWT.sign(user, process.env.SECRET, {
+                    expiresIn: 1800 //expires in 30min
+                })
 
-            resolve({
-                token: token,
-                auth: true
-            })
+                resolve({
+                    token: token,
+                    auth: true
+                })
+            } catch (error) {
+                throw new Error('Ocurred an error during the execution of authenticate function')
+            }
         })
     },
     signOut() {
@@ -25,15 +28,17 @@ module.exports = {
     },
     authorize(authentication) {
         return new Promise((resolve, reject) => {
-            if (authentication.auth === false)
-                reject(new Error('The state of the authentication is false'))
-            else if (!authentication.token)
-                reject(new Error("The token's format is incorrect"))
-            
-            JWT.verify(authentication.token, process.env.SECRET, (err) => {
-                if (err) reject(new Error('Ocurred an error during token verification'))
-                else resolve(true)
-            })
+            try {
+                if (authentication.auth === false) reject(new Error('The state of the authentication is false'))
+                else if (!authentication.token) reject(new Error("The token's format is incorrect"))
+
+                JWT.verify(authentication.token, process.env.SECRET, (err) => {
+                    if (err) reject(new Error('Ocurred an error during token verification'))
+                    else resolve(true)
+                })
+            } catch (error) {
+                throw new Error('Ocurred an error during the execution of authorize function')
+            }
         })
     }
 }
