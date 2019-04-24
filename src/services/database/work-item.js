74a4690e6bfce7680164be2cbe2cd6d57076cdee
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-04-24 20:38:47 -0300

    refatoracao do servico work-item

--- src/services/database/work-item.js
+++ src/services/database/work-item.js
@@ -4,11 +4,12 @@ require('dotenv-safe').config()
 module.exports = {
     insertWorkItem(workItem) {
         return new Promise((resolve, reject) => {
-            if (typeof workItem !== 'object')
-                reject(new Error('The parameter for the insertWorkItem must be an object'))
-            else if (typeof workItem.user !== 'object')
-                reject(new Error('The parameter for the insertWorkItem must have an user inner object'))
-            else {
+            try {
+                if (typeof workItem !== 'object')
+                    reject(new Error('The parameter for the insertWorkItem must be an object'))
+                else if (typeof workItem.user !== 'object')
+                    reject(new Error('The parameter for the insertWorkItem must have an user inner object'))
+
                 workItem.entity = 'workitem'
                 workItem.date = new Date()
 
@@ -19,21 +20,20 @@ module.exports = {
                     json: true
                 }
                 request(requestData, (error, res) => {
-                    if (error) reject(error)
-                    else {
-                        if (res.statusCode != 201) reject(res.statusCode)
-                        else resolve(res.statusCode)
-                    }
+                    if (error | s.statusCode != 201) reject(new Error('The database operation did not work'))
+                    else resolve(true)
                 })
+
+            } catch (error) {
+                reject(new Error('Occurred an error during the execution of insertWorkItem function'))
             }
         })
     },
     getWorkItem(_user) {
         return new Promise((resolve, reject) => {
-            if (typeof _user !== 'object')
-                reject(new Error('The parameter for the getWorkItem must be an object'))
-            else {
-
+            try {
+                if (typeof _user !== 'object')
+                    reject(new Error('The parameter for the getWorkItem must be an object'))
                 const requestData = {
                     method: 'POST',
                     uri: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}/_find`,
@@ -47,39 +47,42 @@ module.exports = {
                 }
 
                 request(requestData, (error, res, body) => {
-                    if (error) reject(error)
-                    else if (res.statusCode != 200) reject(res.statusCode)
+                    if (error | res.statusCode != 200) reject(new Error('The database operation did not work'))
                     else resolve(body.docs)
                 })
+            } catch (error) {
+                reject(new Error('Occurred an error during the execution of getWorkItem function'))
             }
         })
     },
     updateWorkItem(workItem) {
         return new Promise((resolve, reject) => {
-            if (typeof workItem !== 'object')
-                reject(new Error('The parameter for the updateWorkItem must be an object'))
-            else if (typeof workItem.user !== 'object')
-                reject(new Error('The parameter for the insertWorkItem must have an user inner object'))
-            else if (typeof workItem._id === 'undefined')
-                reject(new Error('The parameter for the updateWorkItem must have an _id inner attribute'))
-            else if (typeof workItem._rev === 'undefined')
-                reject(new Error('The parameter for the updateWorkItem must have an _rev inner attribute'))
-            else {
-                const requestData = {
-                    method: 'PUT',
-                    uri: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}/${workItem._id}`,
-                    body: workItem,
-                    json: true
-                }
-
-                request(requestData, (error, res, body) => {
-                    if (error) reject(error)
-                    else {
-                        if (res.statusCode != 201) reject(res.statusCode)
-                        else resolve(res.statusCode)
+            try {
+                if (typeof workItem !== 'object')
+                    reject(new Error('The parameter for the updateWorkItem must be an object'))
+                else if (typeof workItem.user !== 'object')
+                    reject(new Error('The parameter for the insertWorkItem must have an user inner object'))
+                else if (typeof workItem._id === 'undefined')
+                    reject(new Error('The parameter for the updateWorkItem must have an _id inner attribute'))
+                else if (typeof workItem._rev === 'undefined')
+                    reject(new Error('The parameter for the updateWorkItem must have an _rev inner attribute'))
+                else {
+                    const requestData = {
+                        method: 'PUT',
+                        uri: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}/${workItem._id}`,
+                        body: workItem,
+                        json: true
                     }
-                })
+
+                    request(requestData, (error, res) => {
+                        if (error | res.statusCode != 201) reject(new Error('the database operation did not work'))
+                        else resolve(true)
+                    })
+                }
+            } catch (error) {
+                reject(new Error('Occurred an error during the execution of updateWorkItem funcion'))
             }
+
         })
     }
 }
