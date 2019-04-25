8afe599b07a8c052a30e5b76f01dd6c502f902ef
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-04-24 22:19:32 -0300

    refatoracao da rota work-item

--- src/routes/work-item.js
+++ src/routes/work-item.js
@@ -6,14 +6,15 @@ const authenticator = require('./../services/auth')
 module.exports = function (app) {
     router.get('/work-item', async (req, res) => {
         try {
-            const user = req.body
-            const permission = await authenticator.authorize(user.authentication)
-
-            if (permission) res.send(await workItemDB.getWorkItem(user))
-            else throw 501
+            const user = req.body 
             
+            if (await authenticator.authorize(user.authentication))
+                res.status(200).send(await workItemDB.getWorkItem(user))
+            else
+                throw new Error({message: 'User not authenticated'})
+
         } catch (error) {
-            res.writeHead(500)
+            res.status(500).send({message: error.message})
         } finally {
             res.end()
         }
@@ -21,10 +22,17 @@ module.exports = function (app) {
 
     router.post('/work-item', async (req, res) => {
         try {
-            const statusCode = await workItemDB.insertWorkItem(req.body)
-            res.writeHead(statusCode)
+            const {user} = req.body
+
+            if (await authenticator.authorize(user.authentication)){
+                if(await workItemDB.insertWorkItem(req.body))
+                    res.status(200).send({message: 'Work item registered successfully'})
+            }
+            else
+                throw new Error({message: 'User not authenticated'})
+
         } catch (error) {
-            res.writeHead(500)
+            res.status(500).send({message: error.message})
         } finally {
             res.end()
         }
@@ -32,10 +40,16 @@ module.exports = function (app) {
 
     router.put('/work-item', async (req, res) => {
         try {
-            const statusCode = await workItemDB.updateWorkItem(req.body)
-            res.writeHead(statusCode)
+            const {user} = req.body
+
+            if (await authenticator.authorize(user.authentication)){
+                if(await workItemDB.updateWorkItem(req.body))
+                    res.status(200).send({message: 'Work item updated successfully'})
+            }
+            else
+                throw new Error({message: 'User not authenticated'})
         } catch (error) {
-            res.writeHead(error)
+            res.status(500).send({message: error.message})
         } finally {
             res.end()
         }
