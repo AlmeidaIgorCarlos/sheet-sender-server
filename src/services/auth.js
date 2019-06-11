52f0df4157d855f78c675cb2655c30649b8d968e
AlmeidaigorCarlos <almeidaigorcarlos@gmail.com>
2019-06-10 21:18:27 -0300

    refatoracao auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,8 +5,6 @@ module.exports = {
     authenticate(user /*Object*/) {
         return new Promise((resolve, reject) => {
             try {
-                if (typeof user !== 'object') reject(new Error('user must be an object'))
-
                 const token = JWT.sign(user, process.env.SECRET, {
                     expiresIn: 1800 //expires in 30min
                 })
@@ -29,9 +27,6 @@ module.exports = {
     authorize(authentication) {
         return new Promise((resolve, reject) => {
             try {
-                if (authentication.auth === false) reject(new Error('The state of the authentication is false'))
-                else if (!authentication.token) reject(new Error("The token's format is incorrect"))
-
                 JWT.verify(authentication.token, process.env.SECRET, (err) => {
                     if (err) reject(new Error('Ocurred an error during token verification'))
                     else resolve(true)
